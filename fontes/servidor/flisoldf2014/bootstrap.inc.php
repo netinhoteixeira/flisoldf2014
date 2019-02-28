bcf3d35eed3be6f7d8c58458406abf37d86001fd
Francisco Ernesto Teixeira <fco.ernesto@gmail.com>
2019-02-27 23:31:43 -0300

    Remoção de dados de acesso sensíveis

--- fontes/servidor/flisoldf2014/bootstrap.inc.php
+++ fontes/servidor/flisoldf2014/bootstrap.inc.php
@@ -4,7 +4,6 @@ use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
 
 require_once 'vendor/autoload.php';
-require 'lib/piwik/PiwikTracker.php';
 
 // Cria uma simples configuração "padrão" do Doctrine ORM para as anotações (Annotation)
 $estaEmDesenvolvimento = true;
@@ -12,30 +11,16 @@ $configuracao = Setup::createAnnotationMetadataConfiguration(array(__DIR__ . '/d
 
 $opcoesDaConexao = array(
     'driver' => 'pdo_mysql',
-    'host' => '127.0.0.1',
-    'user' => 'netinhoi_flisol',
-    'password' => 'DywnrHvn8Wpdid2saq',
-    'dbname' => 'netinhoi_flisoldf2014'
+    'host' => '*****',
+    'user' => '*****',
+    'password' => '******',
+    'dbname' => '******'
 );
 
 $gerenciadorDeEntidades = EntityManager::create($opcoesDaConexao, $configuracao);
 
 $caminhoFotoOriginal = __DIR__ . '/data/foto/original/';
 $caminhoFotoAmostra = __DIR__ . '/data/foto/amostra/';
-
-// Análise do Tráfego
-$piwikTracker = new PiwikTracker(19, 'http://trafego.francisco.pro');
-$piwikTracker->setUserAgent($_SERVER['HTTP_USER_AGENT']);
-$idioma = explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE']);
-$piwikTracker->setBrowserLanguage($idioma[0]);
-unset($idioma);
-$params = convert_url_query($_SERVER['REQUEST_URI']);
-if (array_key_exists('localTime', $params)) {
-    $piwikTracker->setLocalTime($params['localTime']);
-}
-if ((array_key_exists('screenWidth', $params)) && (array_key_exists('screenHeight', $params))) {
-    $piwikTracker->setResolution($params['screenWidth'], $params['screenHeight']);
-}
 
 /**
  * Returns the url query as associative array
